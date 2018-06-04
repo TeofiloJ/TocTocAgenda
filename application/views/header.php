3988582cc1d1778800c4192ee5b1b090c9b92514
teofilo <frnazgul@gmail.com>
2018-06-04 16:48:01 +0200

    maj css et pages

--- application/views/header.php
+++ application/views/header.php
@@ -6,11 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/my-slider.css">
     <!-- JQUERY -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
     
 
     <title>TocTocAgenda</title>
--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -10,25 +10,114 @@
 			</nav>
 
 <div class="container">
-  <form action="">
+    <div class="row">
+        <div class="col-md-12">
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/contact.css" />
 
-    <label for="fname">Prénom</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
+    
+<div class="container animated fadeIn">
 
-    <label for="lname">Nom</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+  <div class="row">
+    <h1 class="header-title"> Contact </h1>
+    <hr>
+    <div class="col-sm-12" id="parent">
+    	<div class="col-sm-6">
+    	<iframe width="100%" height="320px;" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12893.309291059408!2d-1.5414511655168135!3d47.2071036578621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4805eea8a2d862ad%3A0xac11d6061c877032!2sYnov+Nantes!5e0!3m2!1sfr!2sfr!4v1528123184472"></iframe>
+    	</div>
 
-    <label for="country">Pays</label>
-    <select id="country" name="country">
-      <option value="australia">France</option>
-      <option value="canada">Angleterre</option>
-      <option value="usa">USA</option>
-    </select>
+    	<div class="col-sm-6">
+    		<form action="form.php" class="contact-form" method="post">
+	
+		        <div class="form-group">
+		          <input type="text" class="form-control" id="name" name="nm" placeholder="Name" required="" autofocus="">
+		        </div>
+		    
+		    
+		        <div class="form-group form_left">
+		          <input type="email" class="form-control" id="email" name="em" placeholder="Email" required="">
+		        </div>
+		    
+		      <div class="form-group">
+		           <input type="text" class="form-control" id="phone" onkeypress="return event.charCode >= 48 && event.charCode <= 57" maxlength="10" placeholder="Mobile No." required="">
+		      </div>
+		      <div class="form-group">
+		      <textarea class="form-control textarea-contact" rows="5" id="comment" name="FB" placeholder="Type Your Message/Feedback here..." required=""></textarea>
+		      <br>
+	      	  <button class="btn btn-default btn-send"> <span class="glyphicon glyphicon-send"></span> Send </button>
+		      </div>
+     		</form>
+    	</div>
+    </div>
+  </div>
 
-    <label for="subject">Sujet</label>
-    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+  <div class="container second-portion">
+	<div class="row">
+        <!-- Boxes de Acoes -->
+    	<div class="col-xs-12 col-sm-6 col-lg-4">
+			<div class="box">							
+				<div class="icon">
+					<div class="image"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+					<div class="info">
+						<h3 class="title">MAIL & WEBSITE</h3>
+						<p>
+							<i class="fa fa-envelope" aria-hidden="true"></i> &nbsp gondhiyahardik6610@gmail.com
+							<br>
+							<br>
+							<i class="fa fa-globe" aria-hidden="true"></i> &nbsp www.hardikgondhiya.com
+						</p>
+					
+					</div>
+				</div>
+				<div class="space"></div>
+			</div> 
+		</div>
+			
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+			<div class="box">							
+				<div class="icon">
+					<div class="image"><i class="fa fa-mobile" aria-hidden="true"></i></div>
+					<div class="info">
+						<h3 class="title">CONTACT</h3>
+    					<p>
+							<i class="fa fa-mobile" aria-hidden="true"></i> &nbsp (+91)-9624XXXXX
+							<br>
+							<br>
+							<i class="fa fa-mobile" aria-hidden="true"></i> &nbsp  (+91)-7567065254 
+						</p>
+					</div>
+				</div>
+				<div class="space"></div>
+			</div> 
+		</div>
+			
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+			<div class="box">							
+				<div class="icon">
+					<div class="image"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+					<div class="info">
+						<h3 class="title">ADDRESS</h3>
+    					<p>
+							 <i class="fa fa-map-marker" aria-hidden="true"></i> &nbsp 15/3 Junction Plot 
+							 "Shree Krishna Krupa", Rajkot - 360001.
+						</p>
+					</div>
+				</div>
+				<div class="space"></div>
+			</div> 
+		</div>		    
+		<!-- /Boxes de Acoes -->
+		
+		<!--My Portfolio  dont Copy this -->
+	    
+	</div>
+</div>
 
-    <button id="button" name="validate" class="btn btn-success">Envoyer</button>
+</div>
 
-  </form>
-</div> 
+
+
+
+
+
+</div></div></div>
